e9c8e0725a26dfc69eaa3b9c252f91da490ec8cb
Mani <manipatta747@gmail.com>
2023-06-10 10:04:32 +0530

    Added -y to few lines to prevent interactiveness

--- jenkins.sh
+++ jenkins.sh
@@ -5,13 +5,13 @@ apt-get install gpg
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
 sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get update -y
-apt-get install default-jre
-apt-get install default-jdk
+apt-get install default-jre -y
+apt-get install default-jdk -y
 echo "JAVA_HOME=\"/usr/lib/jvm/java-11-openjdk-amd64\"" >> /etc/environment
 source /etc/environment
 echo $JAVA_HOME
-apt-get install jenkins
-apt-get install curl
+apt-get install jenkins -y
+apt-get install curl -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
